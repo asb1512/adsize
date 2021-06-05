43842a3ca5e91db06289823293c073e423606ac9
asb1512 <andrew@olympicmedia.com>
2021-06-05 19:37:41 -0400

    database reflects changes from last migration, adding foreign key to every instance of AdDimension

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_05_210631) do
+ActiveRecord::Schema.define(version: 2021_06_05_231355) do
 
   create_table "ad_dimensions", force: :cascade do |t|
     t.integer "width"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_06_05_210631) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.integer "platform_id"
   end
 
   create_table "platforms", force: :cascade do |t|
