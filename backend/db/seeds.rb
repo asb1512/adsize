afe5b874a0a87f13599d87d533945485c89e1390
asb1512 <andrew@olympicmedia.com>
2021-06-05 17:15:57 -0400

    added seed data to create ad dimensions for Facebook and associating them to their parent Platform

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Creation of ad platforms
 platform_names = [
   'Facebook',
   'Google',
@@ -16,4 +17,17 @@ platform_names = [
 
 platform_names.each do |name|
   Platform.create(name: name)
+end
+
+# Creation of FB ad dimensions
+fb_ad_dimensions = [
+  {name: 'Standard', width: 1200, height: 628},
+  {name: 'Sponsored Message', width: 1080, height: 1080},
+  {name: 'Standard Carousel', width: 1080, height: 1080}
+]
+
+fb_ad_dimensions.each do |ad|
+  e = AdDimension.create(name: ad.name, width: ad.width, height: ad.height)
+  fb = Platform.find_by(name: 'Facebook')
+  e.platform = fb
 end
