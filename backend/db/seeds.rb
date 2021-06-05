75206ea802ede23f0fbc246de9d56d68c1985e46
asb1512 <andrew@olympicmedia.com>
2021-06-05 16:40:46 -0400

    added seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+platform_names = [
+  'Facebook',
+  'Google',
+  'Yahoo',
+  'Topple',
+  'Display'
+]
+
+platform_names.each do |name|
+  Platform.create(name: name)
+end
