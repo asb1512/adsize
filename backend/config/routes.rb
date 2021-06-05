6fccf1eb5eaceba4ed91a5b3bc16087a586619da
asb1512 <andrew@olympicmedia.com>
2021-06-05 16:50:42 -0400

    updated routes to nest ad_dimensions within platforms

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :platforms
+  resources :platforms, only: [:index, :show] do
+    resources :ad_dimensions, only: [:index, :show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
