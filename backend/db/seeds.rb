3dade9c3f52464ef8c21faea0a048be29bec25eb
asb1512 <andrew@olympicmedia.com>
2021-06-05 18:51:19 -0400

    added Google Ad Dimensions and updated file to delete all instances before creation

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Clear DB each time seed file is run.
+Platform.delete_all
+AdDimension.delete_all
+
 # Creation of ad platforms
 platform_names = [
   'Facebook',
@@ -31,3 +35,35 @@ fb_ad_dimensions.each do |ad|
   fb = Platform.find_by(name: 'Facebook')
   e.platform = fb
 end
+
+# Creation of Google ad dimensions
+google_ad_dimensions = [
+  {name: 'Small Square', width: 200, height: 200},
+  {name: 'Vertical rectangle', width: 240, height: 400},
+  {name: 'Square', width: 250, height: 250},
+  {name: 'Triple Widescreen', width: 250, height: 360},
+  {name: 'Inline Rectangle', width: 300, height: 250},
+  {name: 'Large Rectangle', width: 336, height: 280},
+  {name: 'Netboard', width: 580, height: 400},
+  {name: 'Skyscraper', width: 120, height: 600},
+  {name: 'Wide Skyscraper', width: 160, height: 600},
+  {name: 'Half-Page Ad', width: 300, height: 600},
+  {name: 'Portrait', width: 300, height: 1050},
+  {name: 'Banner', width: 468, height: 60},
+  {name: 'Leaderboard', width: 728, height: 90},
+  {name: 'Top Banner', width: 930, height: 180},
+  {name: 'Large Leaderboard', width: 970, height: 90},
+  {name: 'Billboard', width: 970, height: 250},
+  {name: 'Panorama', width: 980, height: 120},
+  {name: 'Mobile Banner', width: 300, height: 50},
+  {name: 'Mobile Banner V2', width: 320, height: 50},
+  {name: 'Large Mobile Banner', width: 320, height: 100}
+]
+
+google_ad_dimensions.each do |ad|
+  e = AdDimension.create(name: ad.name, width: ad.width, height: ad.height)
+  google = Platform.find_by(name: 'Google')
+  e.platform = google
+end
+
+# Creation of Yahoo ad dimensions
