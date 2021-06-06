81df278faee479e612a99199c0be1bca76666f78
asb1512 <andrew@olympicmedia.com>
2021-06-05 21:47:57 -0400

    created main func that waits until DOM content is loaded. created task() that allows the appending of elements after a set interval

--- frontend/index.js
+++ frontend/index.js
@@ -5,27 +5,39 @@ const body = document.querySelector('body')
 const navBar = document.getElementsByClassName('nav-bar')[0]
 const main = document.querySelector('main')
 
-document.addEventListener('DOMContentLoaded', () => loadPlatforms())
+document.addEventListener('DOMContentLoaded', () => onPageLoad())
 
-// adds logo to page
-const adSizeLogo = document.createElement('img')
-adSizeLogo.setAttribute('id', 'ad-size-logo')
-adSizeLogo.setAttribute('class', 'fade-in-image')
-adSizeLogo.setAttribute('src', 'images/AdSize-Logo.png')
-navBar.appendChild(adSizeLogo)
+function onPageLoad() {
 
-// adds first prompt
+  // allows elements to fade in after a specified period of time
+  const task = async (parent, child) => {
+    await new Promise(r => setTimeout(r, 1000));
+    parent.appendChild(child)
+  }
 
+  // adds logo to page
+  const adSizeLogo = document.createElement('img')
+  adSizeLogo.setAttribute('id', 'ad-size-logo')
+  adSizeLogo.setAttribute('class', 'fade-in-image')
+  adSizeLogo.setAttribute('src', 'images/AdSize-Logo.png')
+  navBar.appendChild(adSizeLogo)
 
-const loadPlatforms = () => {
-  fetch(PLATFORMS_URL)
-  .then(resp => resp.json())
-  .then(json => {
-    json.forEach(platform => renderPlatform(platform))
-  })
-}
+  // adds first prompt
+  const selectPlatformPrompt = document.createElement('p')
+  selectPlatformPrompt.setAttribute('class', 'heading fade-in-image')
+  selectPlatformPrompt.innerHTML = 'Select your platform:'
+  task(main, selectPlatformPrompt)
 
-const renderPlatform = (platform) => {
-  const div = document.createElement('div')
+  const loadPlatforms = () => {
+    fetch(PLATFORMS_URL)
+    .then(resp => resp.json())
+    .then(json => {
+      json.forEach(platform => renderPlatform(platform))
+    })
+  }
 
+  const renderPlatform = (platform) => {
+    const div = document.createElement('div')
+
+  }
 }
