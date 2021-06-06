3f7e9fda45696a4418b6f0011d4c7aee547db15b
asb1512 <andrew@olympicmedia.com>
2021-06-05 20:47:34 -0400

    added functions to select the body element, start running scripts after content is loaded and the fetch call

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,18 @@
 const BASE_URL = "http://localhost:3000"
 const PLATFORMS_URL = `${BASE_URL}/platforms`
 const AD_DIMENSIONS_URL = `${BASE_URL}/ad_dimensions`
+const body = document.querySelector('body')
+
+document.addEventListener('DOMContentLoaded', () => loadPlatforms())
+
+const loadPlatforms = () => {
+  fetch(PLATFORMS_URL)
+  .then(resp => resp.json())
+  .then(json => {
+    json.forEach(platform => renderPlatform(platform))
+  })
+}
+
+const renderPlatform = (platform) => {
+  
+}
