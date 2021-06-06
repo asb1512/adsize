dbccff83dd32d13d8b1a810e4122e8f27723a0be
asb1512 <andrew@olympicmedia.com>
2021-06-05 20:21:20 -0400

    specified the Serializer to be used for nested attributes of AdDimension

--- backend/app/serializers/ad_dimension_serializer.rb
+++ backend/app/serializers/ad_dimension_serializer.rb
@@ -1,4 +1,4 @@
 class AdDimensionSerializer < ActiveModel::Serializer
   attributes :id, :name, :width, :height, :platform
-  belongs_to :platform
+  belongs_to :platform, serializer: PlatformSerializer
 end
