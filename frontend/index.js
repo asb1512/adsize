6bdd381ed26932d2737f38b010dbd59373f8f129
asb1512 <andrew@olympicmedia.com>
2021-06-05 21:20:31 -0400

    added code to place logo on page

--- frontend/index.js
+++ frontend/index.js
@@ -2,8 +2,20 @@ const BASE_URL = "http://localhost:3000"
 const PLATFORMS_URL = `${BASE_URL}/platforms`
 const AD_DIMENSIONS_URL = `${BASE_URL}/ad_dimensions`
 const body = document.querySelector('body')
+const navBar = document.getElementsByClassName('nav-bar')[0]
+const main = document.querySelector('main')
 
 document.addEventListener('DOMContentLoaded', () => loadPlatforms())
+
+// adds logo to page
+const adSizeLogo = document.createElement('img')
+adSizeLogo.setAttribute('id', 'ad-size-logo')
+adSizeLogo.setAttribute('class', 'fade-in-image')
+adSizeLogo.setAttribute('src', 'images/AdSize-Logo.png')
+navBar.appendChild(adSizeLogo)
+
+// adds first prompt
+
 
 const loadPlatforms = () => {
   fetch(PLATFORMS_URL)
@@ -14,5 +26,6 @@ const loadPlatforms = () => {
 }
 
 const renderPlatform = (platform) => {
-  
+  const div = document.createElement('div')
+
 }
