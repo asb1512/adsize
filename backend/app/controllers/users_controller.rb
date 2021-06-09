2f22a35f8b690da72c74fb8c5d17838ed0b58f3e
asb1512 <andrew@olympicmedia.com>
2021-06-09 01:20:39 -0400

    updated render statements to respond with the retrieved or newly created User instance

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -2,11 +2,10 @@ class UsersController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user
-      puts user
-      render status: "200", json: {message: "user successfully authenticated"}
+      render status: "200", json: {message: "user successfully authenticated", user: user}
     else
-      User.create(email: params[:email])
-      render status: "200", json: {message: "user account successfully created"}
+      created_user = User.create(email: params[:email])
+      render status: "200", json: {message: "user account successfully created", user: created_user}
     end
   end
 end
