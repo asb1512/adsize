e38f21ce27af02ced6099cb09c2aaa06f4471b12
asb1512 <andrew@olympicmedia.com>
2021-06-08 23:48:57 -0400

    created users#create to handle authentication

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+    if user
+      puts user
+      render status: "200", json: {message: "user successfully authenticated"}
+    else
+      User.create(email: params[:email])
+      render status: "200", json: {message: "user account successfully created"}
+    end
+  end
 end
