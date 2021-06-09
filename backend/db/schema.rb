39a3448d6fb28fe52b290ca3d44a96bb88fbfed9
asb1512 <andrew@olympicmedia.com>
2021-06-09 01:15:42 -0400

    ran migrations

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_043115) do
+ActiveRecord::Schema.define(version: 2021_06_09_050850) do
 
   create_table "ad_dimensions", force: :cascade do |t|
     t.integer "width"
@@ -25,12 +25,14 @@ ActiveRecord::Schema.define(version: 2021_06_09_043115) do
     t.string "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "list_id"
   end
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "platforms", force: :cascade do |t|
