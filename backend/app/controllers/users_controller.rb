5f981d0e656411ff2bb272da644c0e72f1d3a8ba
asb1512 <andrew@olympicmedia.com>
2021-06-09 01:27:27 -0400

    changed response messages

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -2,10 +2,10 @@ class UsersController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user
-      render status: "200", json: {message: "user successfully authenticated", user: user}
+      render status: "200", json: {message: "Successfully authenticated", user: user}
     else
       created_user = User.create(email: params[:email])
-      render status: "200", json: {message: "user account successfully created", user: created_user}
+      render status: "200", json: {message: "Your account was successfully created", user: created_user}
     end
   end
 end
