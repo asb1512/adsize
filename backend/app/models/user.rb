d7e65d8c5d23711e9552d5234cbc8b1be2789f42
asb1512 <andrew@olympicmedia.com>
2021-06-09 00:58:49 -0400

    corrected incorrect validations for User model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
   # make sure to add data associations once all tables exist
+  # has_many :reports
 end
