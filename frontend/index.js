f0c71565707a04cfac98764bba7a2a822c7bc5bd
asb1512 <andrew@olympicmedia.com>
2021-06-10 23:14:58 -0400

    updated currentUser obj and changed structure of JSON response

--- frontend/index.js
+++ frontend/index.js
@@ -6,7 +6,7 @@ const navBar = document.getElementsByClassName('nav-bar')[0]
 const main = document.querySelector('main')
 
 const jsonResp = []
-const currentUser = {id: 0, email: ""}
+const currentUser = {id: 0, email: "", list_id: 0}
 
 const isEven = int => {
   return (int % 2 === 0)
@@ -83,8 +83,11 @@ function onPageLoad() {
     fetch(USERS_URL, configObj)
     .then(resp => resp.json())
     .then(json => {
-      currentUser.id = json.user.id
-      currentUser.email = json.user.email
+      debugger
+      currentUser.id = json.id
+      currentUser.email = json.email
+      currentUser.list_id = 
+      
       displayUserList()
     })
     .catch(error => {
@@ -105,7 +108,7 @@ function onPageLoad() {
   const displayUserList = () => {
     const mainListDiv = document.createElement('div')
     const listTitle = document.createElement('h3')
-    
+
   }
 
 
