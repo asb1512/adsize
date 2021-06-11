1ee35498a0767523c1f51488ba81b9da63975c08
asb1512 <andrew@olympicmedia.com>
2021-06-10 23:15:35 -0400

    fixed bug with render: json statement and created logic to save new user and associate their list

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -2,10 +2,14 @@ class UsersController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user
-      render status: "200", json: {message: "Successfully authenticated", user: user}
+      render json: user, status: "200", message: "Successfully authenticated"
     else
-      created_user = User.create(email: params[:email])
-      render status: "200", json: {message: "Your account was successfully created", user: created_user}
+      created_user = User.new(email: params[:email])
+      new_list = List.new(title: 'My ToDo List')
+      created_user.list = new_list
+      new_list.save
+      created_user.save
+      render json: created_user, status: "200", message: "Successfully authenticated"
     end
   end
 end
