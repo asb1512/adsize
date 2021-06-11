a283958aa07c0ce5726db6cbd3135715d58f80ab
asb1512 <andrew@olympicmedia.com>
2021-06-10 23:38:14 -0400

    added missing attributes preventing nested resources from displaying in JSON response

--- backend/app/serializers/list_serializer.rb
+++ backend/app/serializers/list_serializer.rb
@@ -1,4 +1,4 @@
 class ListSerializer < ActiveModel::Serializer
-  attributes :id, :title
+  attributes :id, :title, :list_items
   has_many :list_items, serializer: ListItemSerializer
 end
--- backend/app/serializers/user_serializer.rb
+++ backend/app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email
+  attributes :id, :email, :list
   has_one :list, serializer: ListSerializer
 end
