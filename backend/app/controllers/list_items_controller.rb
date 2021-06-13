09b800be0dcacba649eea7821bd0822cc5fc955f
asb1512 <andrew@olympicmedia.com>
2021-06-13 14:31:50 -0400

    initializes a new list item, then locates user, associates the two, and saves to db

--- backend/app/controllers/list_items_controller.rb
+++ backend/app/controllers/list_items_controller.rb
@@ -1,6 +1,10 @@
 class ListItemsController < ApplicationController
   def create
-    binding.pry
     list_item = ListItem.new(message: params[:message])
+    user = User.find_by(id: params[:user_id])
+    user.list.list_items << list_item
+    list_item.save
+    user.save
+    render status: 200, message: "Successfully created user list item."
   end
 end
