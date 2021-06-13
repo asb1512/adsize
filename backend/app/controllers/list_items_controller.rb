ffe6045fb530e78dca80ed55b1d8eccd745bc360
asb1512 <andrew@olympicmedia.com>
2021-06-13 15:39:39 -0400

    created ListItemsController#destroy that deletes list item and sends error message if the item doesn't exist

--- backend/app/controllers/list_items_controller.rb
+++ backend/app/controllers/list_items_controller.rb
@@ -7,4 +7,14 @@ class ListItemsController < ApplicationController
     user.save
     render json: list_item, status: 200, message: "Successfully created user list item."
   end
+
+  def destroy
+    binding.pry
+    list_item = ListItem.find_by(id: params[:id])
+    if list_item
+      list_item.destroy
+    else
+      render status: 400, message: "Request note doesn not exist."
+    end
+  end
 end
