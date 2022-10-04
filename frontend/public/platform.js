c68131ae6f83f5b2daf41ac3be05e836465f998a
asb1512 <andrew@olympicmedia.com>
2022-10-03 23:54:27 -0400

    moved classes to separate files

---
+++ frontend/public/platform.js
@@ -0,0 +1,7 @@
+export default class Platform {
+  constructor(id, name, adDimensions) {
+    this.id = id;
+    this.name = name;
+    this.adDimensions = adDimensions;
+  }
+}
---
+++ frontend/public/user.js
@@ -0,0 +1,7 @@
+export default class User {
+  constructor(id, email, list) {
+    this.id = id;
+    this.email = email;
+    this.list = list;
+  }
+}
